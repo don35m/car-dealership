ae2f7333b04d050b07f96f11589540306cfc84d2
Tyler & Don J Schemmel <dons500@gmail.com>
2015-08-03 14:06:44 -0700

    Add images and default values

--- Cars.php
+++ Cars.php
@@ -3,33 +3,32 @@
 
     class Car
     {
+
         public $make_model;
+        public $image;
         public $price;
         public $miles;
-    }
+        public $color;
 
 
 
-    $porsche = new Car();
-    $porsche->make_model = "2014 Porsche 911";
-    $porsche->price = 114991;
-    $porsche->miles = 7864;
+        function __construct($make_model, $image, $price, $miles, $color = "black")
+        {
+            $this->make_model = $make_model;
+            $this->image = $image;
+            $this->price = $price;
+            $this->miles = $miles;
+            $this->color = $color;
+        }
+      }
 
 
-    $ford = new Car();
-    $ford->make_model = "2011 Ford F450";
-    $ford->price = 55995;
-    $ford->miles = 14241;
+    $porsche = new Car("2014 Porsche 911", "images/porsche.jpeg",  114991, 7864);
+    $ford = new Car("2011 Ford F450", "images/ford.jpeg", 55995, 14241);
+    $lexus = new Car("2013 Lexus RX350", "images/lexus.jpg", 44700, 20000, "red");
+    $mercedes = new Car("Mercedes Benz CLS550", "images/mercedes.jpg", 39900, 37979);
 
-    $lexus = new Car();
-    $lexus->make_model = "2013 Lexus RX 350";
-    $lexus->price = 44700;
-    $lexus->miles = 20000;
 
-    $mercedes = new Car();
-    $mercedes->make_model = "Mercedes Benz CLS550";
-    $mercedes->price = 39900;
-    $mercedes->miles = 37979;
 
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
@@ -46,9 +45,11 @@
         <?php
             foreach ($cars as $car) {
                 echo "<li> $car->make_model </li>";
+                echo "<li><img src= $car->image ></li>";
                 echo "<ul>";
                     echo "<li> $$car->price </li>";
                     echo "<li> Miles: $car->miles </li>";
+                    echo "<li> Color: $car->color </li>";
                 echo "</ul>";
             }
         ?>
